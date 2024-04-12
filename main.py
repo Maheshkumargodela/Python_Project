cbb866649002597126446fa2697680ad7efb5c0a
Maheshkumargodela <158819337+Maheshkumargodela@users.noreply.github.com>
2024-04-12 23:55:43 +0000

    Dynamic website is created

--- main.py
+++ main.py
@@ -1,12 +1,39 @@
-from flask import Flask
+from flask import Flask, render_template, jsonify
 
 main = Flask(__name__)
+name = 'Mahesh Kumar'
+JOBS = [{
+    'id': 1,
+    'title': 'Data Analyst',
+    'location': 'Bengaluru, India',
+    'salary': 'Rs. 10,00,000'
+}, {
+    'id': 2,
+    'title': 'Data Scientist',
+    'location': 'Delhi, India',
+    'salary': 'Rs. 15,00,000',
+}, {
+    'id': 3,
+    'title': 'Frontend Engineer',
+    'location': 'Remote',
+    'salary': 'Rs. 12,00,000',
+}, {
+    'id': 4,
+    'title': 'Backend Engineer',
+    'location': 'San Francisco, USA',
+    'salary': '$120,000',
+}]
 
 
 @main.route('/')
 def index():
-  return "Mahesh"
+  return render_template('index.html', jobs=JOBS, company_name=name)
+
+
+@main.route('/api/jobs')
+def list_jobs():
+  return jsonify(JOBS)
 
 
 if __name__ == '__main__':
-  main.run('0.0.0.0', debug=True)
+  main.run(host='0.0.0.0', port=8080)
